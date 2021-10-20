f06de899d821212137cd5d7f1dc6dd6fa671531d
venkatmallisetti <92291069+venkatmallisetti@users.noreply.github.com>
2021-10-20 12:28:59 +0530

    Add files via upload

---
+++ venkat1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int number1;
+	float number2,sum;
+	number1=7;
+	number2=1.3;
+	sum = number1+number2;
+	printf("Addition of %d and %f is %f",number1,number2,sum);
+}
+
