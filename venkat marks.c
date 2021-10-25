202ca4d9efaa26e08c86dd359fb1544ddef3ab2a
venkatmallisetti <92291069+venkatmallisetti@users.noreply.github.com>
2021-10-25 12:07:41 +0530

    Add files via upload

---
+++ venkat marks.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int physics;
+	int chemistry;
+	int maths;
+	int english;
+	int total;
+	physics=55;
+	chemistry=52;
+	maths=75;
+	english=90;
+	total=chemistry+physics+maths+english;
+	printf("marks in physics, %d\n",physics);
+	printf("marks in chemistry, %d\n",chemistry);
+	printf("marks in maths, %d\n",maths);
+	printf("marks in english,%d\n",english);
+	printf("marks in total,%d",total);
+	}
