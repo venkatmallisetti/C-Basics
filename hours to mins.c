ea895d13bd638cd8f5755e1d48b453be93c4bb15
venkatmallisetti <92291069+venkatmallisetti@users.noreply.github.com>
2021-11-03 12:11:39 +0530

    Add files via upload

---
+++ hours to mins.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int hours,mins1,mins2,minutes;
+	
+	hours=12;
+	mins1=60;
+	mins2=10;
+	minutes=hours*mins1+mins2;
+	printf(" %d hours %d mins2 is %d minutes",hours,mins2,minutes);
+	}
