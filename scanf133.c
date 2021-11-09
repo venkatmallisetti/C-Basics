7327ed1b5b3b95f861dfc3c5c7c4839cd5e98d4a
venkatmallisetti <92291069+venkatmallisetti@users.noreply.github.com>
2021-11-09 11:51:01 +0530

    Add files via upload

---
+++ scanf133.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	printf("Enter four numbers");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	printf("Enter 1st number");
+	scanf("%d",&a);
+	printf("Enter 2nd number");
+	scanf("%d",&b);
+	printf("Enter 3rd number");
+	scanf("%d",&c);
+	printf("Enter 4th number");
+	scanf("%d",&d);
+	printf("product of four numbers:%d%d%d%d",a*b*c*d);
+}
